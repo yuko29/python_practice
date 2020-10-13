b829101aa10e090b29e4629616c2314fbffd371b
yuko29 <yuko29.cs07@gmail.com>
2020-10-14 00:24:43 +0800

    Q4 finish

---
+++ 4.py
@@ -0,0 +1,17 @@
+h = int(input("Unput a number: "))
+
+
+for i in range(1, h+1, 2):
+    space = int((h-i)/2)
+    s = ' ' * space
+    p = "*" * i
+    row = s+p+s
+    print(row)
+
+for i in range(h-2, 0, -2):
+    space = int((h-i)/2)
+    s = ' ' * space
+    p = "*" * i
+    row = s+p+s
+    print(row)
+                        
